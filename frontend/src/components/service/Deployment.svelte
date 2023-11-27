8dd3deb878f468db79dcfb5cfb852537a6f0538e
Roberto Huertas <roberto.huertas@outlook.com>
2023-11-27 21:33:51 +0100

    chore(ui): remove unused import

--- frontend/src/components/service/Deployment.svelte
+++ frontend/src/components/service/Deployment.svelte
@@ -1,17 +1,12 @@
 <script lang="ts">
-	import type { Deployment } from "$lib/models";
-	import { onMount } from "svelte";
-	import DeploymentIcon from "./DeploymentIcon.svelte";
+	import type { Deployment } from '$lib/models';
+	import DeploymentIcon from './DeploymentIcon.svelte';
 
-  export let deployment: Deployment;
+	export let deployment: Deployment;
 </script>
+
 <p class="font-semibold mb-1">Last deployment</p>
 <div class="flex justify-start mb-2">
-  <DeploymentIcon status={deployment.status}/>
-  <p class="ml-2">{deployment.createdAt.toUTCString()}</p>
+	<DeploymentIcon status={deployment.status} />
+	<p class="ml-2">{deployment.createdAt.toUTCString()}</p>
 </div>
-
-
-
-
-
